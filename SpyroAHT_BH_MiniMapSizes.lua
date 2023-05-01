57fe7c12fbd68f7e1ef9d828add79ce7f1bdc943
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2023-05-01 16:31:30 +0200

    Add files via upload

---
+++ SpyroAHT_BH_MiniMapSizes.lua
@@ -0,0 +1,94 @@
+local BH_MiniMapSizes = {
+	[1] = {
+		Name = "Dragon Village",
+		X = 29,
+		Y = 21,
+		Size = 609
+	},
+	[2] = {
+		Name = "Crocovile Swamp",
+		X = 24,
+		Y = 18,
+		Size = 432
+	},
+	[3] = {
+		Name = "Dragonfly Falls",
+		X = 45,
+		Y = 37,
+		Size = 1665
+	},
+	[4] = {
+		Name = "Coastal Remains",
+		X = 51,
+		Y = 28,
+		Size = 1428
+	},
+	[5] = {
+		Name = "Sunken Ruins",
+		X = 34,
+		Y = 22,
+		Size = 748
+	},
+	[6] = {
+		Name = "Cloudy Domain",
+		X = 30,
+		Y = 24,
+		Size = 720
+	},
+	[7] = {
+		Name = "Frostbite Village",
+		X = 27,
+		Y = 43,
+		Size = 1161
+	},
+	[8] = {
+		Name = "Gloomy Glacier",
+		X = 19,
+		Y = 44,
+		Size = 836
+	},
+	[9] = {
+		Name = "Ice Citadel",
+		X = 33,
+		Y = 22,
+		Size = 726
+	},
+	[10] = {
+		Name = "Stormy Beach",
+		X = 22,
+		Y = 26,
+		Size = 572
+	},
+	[11] = {
+		Name = "Molten Mount",
+		X = 37,
+		Y = 43,
+		Size = 1591
+	},
+	[12] = {
+		Name = "Magma Falls Top",
+		X = 7,
+		Y = 19,
+		Size = 133
+	},
+	[13] = {
+		Name = "Magma Falls Bottom",
+		X = 21,
+		Y = 17,
+		Size = 357
+	},
+	[14] = {
+		Name = "Dark Mine",
+		X = 22,
+		Y = 23,
+		Size = 506
+	},
+	[15] = {
+		Name = "Red's Laboratory",
+		X = 34,
+		Y = 24,
+		Size = 816
+	}
+}
+
+return BH_MiniMapSizes
